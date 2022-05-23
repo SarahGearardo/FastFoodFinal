3f5d25c0635c96acad9b7944d9256b5ab8882e8d
Melissa <melisochka926@gmail.com>
2022-05-22 21:28:09 -0500

    Update

--- backup.sql
+++ backup.sql
@@ -26,11 +26,27 @@ CREATE TABLE Yelp_Restaurants (
 
 drop table joined_tables cascade;
 
--- Joining the two tables on Zip Code
--- Joining the two tables on Zip Code
+
+-- Joining the two tables on Zip Code and counting density
 SELECT zip_code_h, count(restaurant), cast(median as money)
 INTO joined_tables
 FROM yelp_restaurants as yp
 INNER JOIN Household_Income as hi
 ON (yp.zip_code_Y = hi.zip_code_H)
 GROUP BY zip_code_h;
+
+select * from joined_tables;
+
+drop table res_type cascade;
+ 
+-- Joining the two tables on Zip Code with restaurant type
+SELECT zip_code_h, res_type, cast(median as money)
+INTO res_type
+FROM yelp_restaurants as yp
+INNER JOIN Household_Income as hi
+ON (yp.zip_code_Y = hi.zip_code_H)
+GROUP BY zip_code_h, res_type
+ORDER BY zip_code_h;
+
+
+select * from res_type;
